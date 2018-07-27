47ad97adeafc2957ae6d3c0ead65a78cd5513237
jingyuan94 <jingyuan0520@live.com.my>
2018-07-27 17:35:25 +0800

    Update

--- src/TruckLoading.java
+++ src/TruckLoading.java
@@ -5,18 +5,29 @@ import java.util.Stack;
 
 public class TruckLoading {
 	public static void main(String[] args) {
+		int[] weights = new int[]{9,2,10,2,2,8,1,7,2,7};
+		int maxBinCapacity = 15;
+		testFirstFit(weights, maxBinCapacity);
+	}
+	
+	public static Stack<Parcel> initializeParcels(int[] weights) {
 		Stack<Parcel> parcels = new Stack<Parcel>();
-		int [] weights = {9,2,10,2,2,8,1,7,2,7};
 		for(int i = 0 ; i < weights.length; i++) {
 			parcels.push(new Parcel(weights[i]));
 		}
-		// Random rand = new Random();
-		//for (int i = 0; i < 10; i++) {
-		//	parcels.push(new Parcel(rand.nextInt(10) + 1));
-		//}
+		return parcels;
+	}
+	
+	public static void testFirstFit(int[] weights, int maxBinCapacity) {
+		System.out.println("Test first fit");
+		System.out.println("Initial parcels");
+		
+		Stack<Parcel> parcels = initializeParcels(weights);
 		System.out.println(parcels);
-		MyLinkedList<Bin> result = Solution.firstFit(parcels, 15);
+		System.out.println("Bin maximun capacity is " + maxBinCapacity);
+		System.out.println("Resulting bins");
+		MyLinkedList<Bin> result = Solution.firstFit(parcels, maxBinCapacity);
 		System.out.println(result.toString());
 	}
-
+	
 }
